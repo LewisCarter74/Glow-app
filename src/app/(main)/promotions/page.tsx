9cefe5c73d337e286092a4dc539767f4dd95ac87
Kamaina Kadogo <kamainakadogo@gmail.com>
2025-07-13 12:07:13 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(main)/promotions/page.tsx
+++ src/app/(main)/promotions/page.tsx
@@ -1,5 +1,6 @@
+
 import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Gift, Star, TicketPercent } from 'lucide-react';
 import Link from 'next/link';
 
