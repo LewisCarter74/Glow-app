7cf1c56b47f21feb662345343628a993c7abc5e7
wacuesther206 <wacuesther206@gmail.com>
2025-07-22 17:07:10 +0000

    1

--- backend/salon/forms.py
+++ backend/salon/forms.py
@@ -2,17 +2,10 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
-class CustomUserCreationForm(forms.ModelForm):
-    class Meta:
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone_number', 'role', 'profile_image')
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
 
 class CustomUserChangeForm(forms.ModelForm):
     class Meta:
