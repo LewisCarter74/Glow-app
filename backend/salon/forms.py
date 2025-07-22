6aba46c3499f12b7367a9a383fc09eacad3cf818
wacuesther206 <wacuesther206@gmail.com>
2025-07-22 17:35:54 +0000

    2

--- backend/salon/forms.py
+++ backend/salon/forms.py
@@ -3,13 +3,13 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    # Override the password2 field to remove it
+    # Override the password2 field to remove it for a simpler creation form
     password2 = None
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone_number', 'role', 'profile_image', 'password')
+        fields = ('email', 'role', 'password', 'profile_image')
 
 class CustomUserChangeForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone_number', 'role', 'profile_image', 'is_active', 'is_staff')
+        fields = ('email', 'role', 'profile_image', 'is_active', 'is_staff')
